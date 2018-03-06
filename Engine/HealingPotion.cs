5d4b8870463e186c54cefff69543e88217de3824
Friendscover <tobiaskloepper1@web.de>
2018-03-06 23:01:23 +0100

    Projektdateien hinzufügen.

---
+++ Engine/HealingPotion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine
+{
+    public class HealingPotion : Item      // Child class von Item -> Item abstrakter und Healing Potion ist Differenzierter?
+    {
+        
+        public int AmountToHeal { get; set; }
+
+        public HealingPotion(int id, string name, string namePlural, int amountToHeal) : base(id,name,namePlural)
+        {
+            AmountToHeal = amountToHeal;
+        }
+    }
+
+}
